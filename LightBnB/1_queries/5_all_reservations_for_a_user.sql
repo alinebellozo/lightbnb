cda816392fd9293adc7d3cb9acd8f05232352ddc
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2023-06-15 16:18:42 -0700

    Update some files to fix bugs:
    - No longer receiving an error when searching minimum and maximum cost;
    - No longer receiving an error when searching for a minimum rating.

--- LightBnB/1_queries/5_all_reservations_for_a_user.sql
+++ LightBnB/1_queries/5_all_reservations_for_a_user.sql
@@ -2,6 +2,7 @@ SELECT reservations.id, reservations.start_date, properties.title, properties.co
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
 GROUP BY properties.id, reservations.id
 ORDER BY start_date
 LIMIT 10;
--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -104,21 +104,26 @@ const addUser = function (user) {
  */
 const getAllReservations = function (guest_id, limit = 10) {
   return pool
-  .query(`
-  SELECT reservations.*, properties.*
+    .query(
+      `
+  SELECT reservations.*, properties.*, AVG(rating) AS average_rating
   FROM reservations
   JOIN properties ON reservations.property_id = properties.id
-  WHERE guest_id = $1
-  LIMIT $2
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2 
   `,
-  [guest_id, limit]
-  )
-  .then((result) => {
-    return result.rows;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+      [guest_id, limit]
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
@@ -133,66 +138,45 @@ const getAllProperties = (options, limit = 10) => {
 
   // 1. Setup an array to hold any parameters that may be available for the query
   const queryParams = [];
-
-  // 2. Start the query with all information that comes before the WHERE clause
   let queryString = `
   SELECT properties.*, AVG(property_reviews.rating) AS average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
+  WHERE 1 = 1 
   `;
 
-  // 3. Check if the filters have been passed in as an option
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length}`;
+    queryString += `AND city LIKE $${queryParams.length} `;
   }
 
   if (options.owner_id) {
-    queryParams.push(options.owner_id);
-    if (queryParams.length === 1) {
-      queryString += `WHERE owner_id = $${queryParams.length}`;
-    } else {
-      queryString += `AND owner_id = $${queryParams.length}`;
-    }
+    queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id = $${queryParams.length} `;
   }
 
-  if (options.minimum_price_per_night) {
-    queryParams.push(
-      options.minimum_price_per_night * 100
-    );
-    if (queryParams.length === 2) {
-      queryString += `WHERE cost_per_night >= $${queryParams.length}`;
-    }
-  };
-
-  if (options.maximum_price_per_night) {
-    queryParams.push(options.maximum_price_per_night * 100);
-    if (queryParams.length === 3) {
-      queryString += `WHERE cost_per_night <= $${queryParams.length}`;
-    }
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100)
+    queryParams.push(options.maximum_price_per_night * 100)
+    queryString += `AND (cost_per_night >= $${queryParams.length-1} AND cost_per_night <= $${queryParams.length}) `;
   }
+
+  queryString += `GROUP BY properties.id `;
 
   if (options.minimum_rating) {
-    queryParams.push(options.minimum_rating);
-    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+    queryParams.push(options.minimum_rating)
+    queryString += `HAVING avg(rating) >= $${queryParams.length} `;
   }
 
-  // 4. Add any query that comes after the WHERE clause
   queryParams.push(limit);
-  
   queryString += `
-  GROUP BY properties.id
   ORDER BY cost_per_night
-  LIMIT $${queryParams.length}
+  LIMIT $${queryParams.length};
   `;
-  
-  // 5. Console log everything just to make sure we've done it right
-  console.log(queryString, queryParams);
 
-  // 6. Run the query
-  return pool
-  .query(queryString, queryParams)
-  .then((result) => result.rows)
+  console.log(queryString, queryParams);
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows)
   .catch((err) => {
     console.log(err.message);
   });
@@ -225,7 +209,7 @@ const addProperty = function (property) {
   .query(`
   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-  RETURNING *
+  RETURNING * 
   `,
   objValues
   )
